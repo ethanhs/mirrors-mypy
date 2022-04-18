a0619967f0927f77a9235e119fc68c8bbce87b97
Ethan Smith <ethan@ethanhs.me>
2022-04-18 12:36:43 -0700

    Update to use correct requirement

--- setup.py
+++ setup.py
@@ -6,5 +6,5 @@ from setuptools import setup
 setup(
     name='pre_commit_placeholder_package',
     version='0.0.0',
-    install_requires=['git+https://github.com/python/mypy'],
+    install_requires=['mypy @ git+https://github.com/python/mypy.git@9cab2964a186d7e71567a1528fcc956f9eecebac'],
 )
